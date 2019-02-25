52a504d848f6f83e61eb5fb1e1c1461d4e42afd9
ilya-as <aslamovi@gmail.com>
2019-02-25 16:39:13 +0300

    2. Не перекрывать equals hashCode[#102330]

---
+++ chapter_001/src/test/java/ru/job4j/map/UserTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.map;
+
+import org.junit.Test;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Ilya Aslamov
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class UserTest {
+
+    /**
+     * Test to add map .
+     */
+    @Test
+    public void notOverrideEqualsHashcode() {
+        Calendar calendar = new GregorianCalendar(2017, Calendar.JANUARY, 25);
+        User user1 = new User("Petr", 1, calendar);
+        User user2 = new User("Petr", 1, calendar);
+        Map<User, Object> map = new HashMap<>();
+        map.put(user1, "user1");
+        map.put(user2, "user2");
+        System.out.println("map: " + map);
+        System.out.println("hashcode user1: " + map.get(user1).hashCode());
+        System.out.println("hashcode user2: " + map.get(user2).hashCode());
+    }
+}
