415691ca2798277e2d0ebe506a5636feb4ce56c1
ilya-as <aslamovi@gmail.com>
2019-04-29 13:27:41 +0300

    1. Проверить байтовый поток[#102447]

---
+++ chapter_002/src/main/java/ru/job4j/io/Config.java
@@ -0,0 +1,49 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
+
+public class Config {
+    private final String path;
+    private final Map<String, String> values = new HashMap<String, String>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    public void load() {
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines().filter(line ->!line.startsWith("#"))
+                    .filter(line ->!line.equals("")).forEach(s -> {String[] line = s.split("=");
+                values.put(line[0],line[1]);});
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String value(String key) {
+        String value = values.get(key);
+        if (value != null) {
+            return value;
+        }
+        throw new UnsupportedOperationException("Don't impl this method yet!");
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner out = new StringJoiner(System.lineSeparator());
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines().forEach(out::add);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return out.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Config("app.properties"));
+    }
+}
---
+++ chapter_002/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,23 @@
+package ru.job4j.io;
+
+import org.junit.Test;
+
+import java.io.File;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class ConfigTest {
+    @Test
+    public void whenCheckConfig() {
+        File file = new File("src/main/resources/app.properties");
+        String absolutePath = file.getAbsolutePath();
+        Config config = new Config(absolutePath);
+        config.load();
+        assertThat(config.value("hibernate.dialect"), is("org.hibernate.dialect.PostgreSQLDialect"));
+        assertThat(config.value("hibernate.connection.url"), is("jdbc:postgresql://127.0.0.1:5432/trackstudio"));
+        assertThat(config.value("hibernate.connection.driver_class"), is("org.postgresql.Driver"));
+        assertThat(config.value("hibernate.connection.username"), is("postgres"));
+        assertThat(config.value("hibernate.connection.password"), is("password"));
+    }
+}
